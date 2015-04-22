847816ad2294b735f46213e11c5d2621c1725820
Carlos A Michelen Strofer <c.michelen.strofer@gmail.com>
2015-04-22 12:01:30 -0600

    new AQWA parser

--- bemio/io/aqwa.py
+++ bemio/io/aqwa.py
@@ -14,176 +14,199 @@
 
 import numpy as np
 
-try:
+from bemio.data_structures import bem
 
-    from astropy.io import ascii
-
-except:
-
-    raise Exception('The astropy module must be installed. Try pip install astropy')
-
-from bemio.data_structures import bem as hd
+from math import ceil
 
 class AqwaOutput(object):
     '''
     Class to read and interact with AQWA simulation data
-    Inputs:
-    out_file: name of the AQWA output file
-    Outputs:
-    None
+    **Inputs:**
+
+    * out_file: name of the AQWA output file
+    
+    **Outputs:**
+    
+    * None
+    
     '''
     def __init__(self,out_file):
-        self.files = hd.generate_file_names(out_file)
-        
+        self.files = bem.generate_file_names(out_file)
+
         self.data = {}        
-        
-        self.n_bods = 0
 
-        self.code = 'AQWA'
- 
-        self.readOutFile()
-        
-        
+        self._read()
 
-    def readOutFile(self):
-        
+    def _read(self):
+        code = 'AQWA'
+
         with open(self.files['out'],'r') as fid:
-            self.raw = fid.readlines()
+            raw = fid.readlines()
 
-        body_num = 0
-        body_num2 = 0
+        first_line = 0
 
-        
-        for i, line in enumerate(self.raw):
-            if 'WATER  DEPTH  . . . . . . . . . . . . . . . . =' in line:
-                water_depth = np.array(self.raw[i].split())[-1].astype(float)
-                density = np.array(self.raw[i+2].split())[-1].astype(float)
-                gravity = np.array(self.raw[i+4].split())[-1].astype(float)
-            if '1. STIFFNESS MATRIX AT THE CENTRE OF GRAVITY' in line:
-                self.data[self.n_bods] = hd.HydrodynamicData()
-                cob = []                
-                self.data[self.n_bods].cg = np.array(self.raw[i+2].split())[np.ix_([2,4,6])].astype(float)
-                self.data[self.n_bods].disp_vol = np.array(self.raw[i+11].split())[-1].astype(float)
-                cob.append(np.array(self.raw[i+14].split())[-1].astype(float))
-                cob.append(np.array(self.raw[i+15].split())[-1].astype(float))
-                cob.append(np.array(self.raw[i+16].split())[-1].astype(float))
-                self.data[self.n_bods].cb = np.array(cob)
-                self.data[self.n_bods].wp_area = np.array(self.raw[i+19].split())[-1].astype(float)
-                self.data[self.n_bods].water_depth = water_depth
-                self.data[self.n_bods].g = gravity 
-                self.data[self.n_bods].rho = density
-                self.data[self.n_bods].name = 'body' + str(self.n_bods)
-                self.data[self.n_bods].bem_code = self.code
-                self.data[self.n_bods].bem_raw_data = self.raw
-                self.n_bods += 1
-            
-            if 'AT THE FREE-FLOATING EQUILIBRIUM POSITION' in line:
-                runLoop = True
-                period = []
-                freq = []
-                kMatrix = []
-                
-                ind = 31
-                
-                self.data[body_num].buoy_force = float(self.raw[i+3].split()[-1])
-                
-                kMatrix.append(np.array(self.raw[i+16].split()[1:]).astype(float))
-                kMatrix.append(np.array(self.raw[i+18].split()[1:]).astype(float))
-                kMatrix.append(np.array(self.raw[i+20].split()[1:]).astype(float))
-                kMatrix.append(np.array(self.raw[i+22].split()[1:]).astype(float))
-                kMatrix.append(np.array(self.raw[i+24].split()[1:]).astype(float))
-                kMatrix.append(np.array(self.raw[i+26].split()[1:]).astype(float))
-                
-                self.data[body_num].k = np.array(kMatrix)                
-                count = 0
-                
-                while runLoop is True:  
-                    am = []
-                    rd = []
-                    if self.raw[i+ind+45].split()[0] != 'WAVE':
-                        runLoop = False
-                        
-                    period.append(self.raw[i+ind].split()[3])
-                    freq.append(self.raw[i+ind].split()[-1])
-                    
-                    am.append(np.array(self.raw[i+ind+10].split()[1:]).astype(float))
-                    am.append(np.array(self.raw[i+ind+12].split()[1:]).astype(float))
-                    am.append(np.array(self.raw[i+ind+14].split()[1:]).astype(float))
-                    am.append(np.array(self.raw[i+ind+16].split()[1:]).astype(float))
-                    am.append(np.array(self.raw[i+ind+18].split()[1:]).astype(float))
-                    am.append(np.array(self.raw[i+ind+20].split()[1:]).astype(float))
-                    am = np.array(am).reshape(6,6,1)
-                    
-                    rd.append(np.array(self.raw[i+ind+30].split()[1:]).astype(float))
-                    rd.append(np.array(self.raw[i+ind+32].split()[1:]).astype(float))
-                    rd.append(np.array(self.raw[i+ind+34].split()[1:]).astype(float))
-                    rd.append(np.array(self.raw[i+ind+36].split()[1:]).astype(float))
-                    rd.append(np.array(self.raw[i+ind+38].split()[1:]).astype(float))
-                    rd.append(np.array(self.raw[i+ind+40].split()[1:]).astype(float))
-                    rd = np.array(rd).reshape(6,6,1)
+        for i, line in enumerate(raw):
 
-                    if count is 0:
-                        amAll = am
-                        rdAll = rd
+            if (first_line == 0) and (line[0] == "*"):
+                continue
+            else: 
+                first_line += 1
+            if first_line == 1:
+                tmp = np.array(raw[i].split()).astype(np.float)
+                num_bodies = int(tmp[0])
+                num_wave_directions = int(tmp[1])
+                num_frequencies = int(tmp[2])
+                num_lines_wave_directions = int(ceil(num_wave_directions/6.))
+                num_lines_frequencies = int(ceil(num_frequencies/6.))
+                for j in range(num_lines_wave_directions):
+                    if j == 0:
+                        wave_directions = tmp[3:]
                     else:
-                        amAll = np.append(amAll,am,axis=2)
-                        rdAll = np.append(rdAll,rd,axis=2)
-                    
-                    ind += 45
-                    count += 1
-                    
-                self.data[body_num].T = np.array(period).astype(float)
-                self.data[body_num].w = np.array(freq).astype(float)
-                self.data[body_num].am.all = amAll
-                self.data[body_num].rd.all = rdAll
-                self.data[body_num].am.infFreq = self.data[body_num].am.all[:,:,-1]
-                self.data[body_num].am.zeroFreq = self.data[body_num].am.all[:,:,0]
+                        tmp = np.array(raw[i+j].split()).astype(np.float)
+                        np.append(wave_directions,tmp)
+                for j in range(num_lines_frequencies):
+                    tmp = np.array(raw[i + num_lines_wave_directions + j].split()).astype(np.float)
+                    if j == 0:
+                        frequencies = tmp
+                    else:
+                        frequencies = np.append(frequencies,tmp)
 
-                self.data[body_num].nW = np.size(self.data[body_num].w)
-                self.data[body_num].body_num = body_num
 
-                body_num += 1
+            if 'GENERAL' in line:
+                tmp = np.array(raw[i+1].split()).astype(np.float)
+                water_depth = tmp[0]
+                density = tmp[1]
+                gravity = tmp[2]
+                symmetry = tmp[3]
 
-            if '* * * * H Y D R O D Y N A M I C   P A R A M E T E R S   F O R   S T R U C T U R E'  in line:
-                if 'FROUDE KRYLOV + DIFFRACTION FORCES-VARIATION WITH WAVE PERIOD/FREQUENCY' in self.raw[i+4]:
-                    temp3 = ascii.read(self.raw[i+12:i+11+np.size(self.data[0].w)]) # Change this index from 0 to the correct index
-                    temp = self.raw[i+11].split()
-                    temp2 = float(temp.pop(2))
-                    exMag = []
-                    exPhase = []
-                    exAll = {}
-                    if temp2 == 180:
-                        exAll[body_num2] = temp3
-                        exAll[body_num2].add_row(temp)
-                        temp = exAll[body_num2].copy()
-                        exAll[body_num2][0] = temp[-1]
-                        for k,line in enumerate(exAll[body_num2]):
-                            if k > 0:
-                                exAll[body_num2][k] = temp[k-1]  
-                        for m,freq in enumerate(exAll[body_num2].field(1)):
-                            exMag.append(np.array([exAll[body_num2].field(2)[m],
-                                                       exAll[body_num2].field(4)[m],
-                                                       exAll[body_num2].field(6)[m],
-                                                       exAll[body_num2].field(8)[m],
-                                                       exAll[body_num2].field(10)[m],
-                                                       exAll[body_num2].field(12)[m]]))
-                            exPhase.append(np.array([exAll[body_num2].field(3)[m],
-                                                       exAll[body_num2].field(5)[m],
-                                                       exAll[body_num2].field(7)[m],
-                                                       exAll[body_num2].field(9)[m],
-                                                       exAll[body_num2].field(11)[m],
-                                                       exAll[body_num2].field(13)[m]]))
+            if 'DRAFT' in line:
+                draft = {}
+                for iBod in range(num_bodies):
+                    tmp = np.array(raw[i + iBod + 1].split()).astype(np.float)
+                    tmp2 = int(tmp[0])
+                    draft[tmp2] = tmp[1]
 
-                        self.data[body_num2].waveDir = np.deg2rad(temp2)
-                        self.data[body_num2].ex.mag = np.array(exMag)
-                        self.data[body_num2].ex.phase = np.array(exPhase)
-                        self.data[body_num2].ex.re = self.data[body_num2].ex.mag*np.cos(np.deg2rad(self.data[body_num2].ex.phase))
-                        self.data[body_num2].ex.im  = self.data[body_num2].ex.mag*np.sin(np.deg2rad(self.data[body_num2].ex.phase))
-                        body_num2 += 1
-        
-    
-    def writeWecSimHydroData(self):
-        hd.writeWecSimHydroData(self.data,self.files['wecSim'])     
-        
-    def writeHdf5(self):
-        hd.writeHdf5(self.data,self.files['hdf5'])
+            if 'COG' in line:
+                cg = {}
+                for iBod in range(num_bodies):
+                    tmp = np.array(raw[i + iBod + 1].split()).astype(np.float)
+                    tmp2 = int(tmp[0])
+                    cg[tmp2] = tmp[1:]
+
+            if line.split()[0] == 'MASS':
+                mass_matrix = {}
+                for iBod in range(num_bodies):
+                    for iRow in range(6):
+                        tmp = np.array(raw[i + iBod*6 + iRow + 1].split()).astype(np.float)
+                        if iRow == 0:
+                            tmp2 = int(tmp[0])
+                            mass_matrix[tmp2] = tmp[1:]
+                        else:
+                            mass_matrix[tmp2] = np.vstack([mass_matrix[tmp2],tmp])
+
+            if 'HYDSTIFFNESS' in line:
+                stiffness_matrix = {}
+                for iBod in range(num_bodies):
+                    for iRow in range(6):
+                        tmp = np.array(raw[i + iBod*6 + iRow + 1].split()).astype(np.float)
+                        if iRow == 0:
+                            tmp2 = int(tmp[0])
+                            stiffness_matrix[tmp2] = tmp[1:]
+                        else:
+                            stiffness_matrix[tmp2] = np.vstack([stiffness_matrix[tmp2],tmp])
+
+            if 'ADDEDMASS' in line:
+                added_mass = {}
+                for iBod1 in range(num_bodies):
+                    for iBod2 in range(num_bodies):
+                        for iFreq in range(num_frequencies):
+                            for iRow in range(6):
+                                tmp = np.array(raw[i + iBod1*(num_bodies*num_frequencies*6) + iBod2*num_frequencies*6 + iFreq*6 + iRow + 1].split()).astype(np.float)
+                                if (iBod2==0) and (iFreq==0) and (iRow==0):
+                                    tmp2 = tmp[0:3].astype(np.float).astype(np.int)
+                                    added_mass[tmp2[0]] = np.zeros([6,6*num_bodies,num_frequencies])
+                                    added_mass[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp[3:]
+                                elif iRow == 0:
+                                    tmp2 = tmp[0:3].astype(np.float).astype(np.int)
+                                    added_mass[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp[3:] 
+                                else:
+                                    added_mass[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp
+
+            if 'DAMPING' in line:
+                radiation_damping = {}
+                for iBod1 in range(num_bodies):
+                    for iBod2 in range(num_bodies):
+                        for iFreq in range(num_frequencies):
+                            for iRow in range(6):
+                                tmp = np.array(raw[i + iBod1*(num_bodies*num_frequencies*6) + iBod2*num_frequencies*6 + iFreq*6 + iRow + 1].split()).astype(np.float)
+                                if (iBod2==0) and (iFreq==0) and (iRow==0):
+                                    tmp2 = tmp[0:3].astype(np.float).astype(np.int)
+                                    radiation_damping[tmp2[0]] = np.zeros([6,6*num_bodies,num_frequencies])
+                                    radiation_damping[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp[3:]
+                                elif iRow == 0:
+                                    tmp2 = tmp[0:3].astype(np.float).astype(np.int)
+                                    radiation_damping[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp[3:] 
+                                else:
+                                    radiation_damping[tmp2[0]][:,iBod2*6+iRow,iFreq] = tmp
+
+            if 'FIDD' in line:
+                fidd = {}
+                for iBod in range(num_bodies):
+                    tmp = np.array(raw[i + iBod + 1].split()).astype(np.float)
+                    tmp2 = int(tmp[0])
+                    fidd[tmp2] = tmp[1:]
+
+            if 'FORCERAO' in line:
+                excitation_magnitude = {}
+                excitation_phase = {}
+                for iBod in range(num_bodies):
+                    for iDir in range(num_wave_directions):
+                        for iFreq in range(num_frequencies):
+                            tmp1_1 = np.array(raw[i + iBod*(num_wave_directions*num_frequencies*2) + iDir*num_frequencies*2 + iFreq*2 + 1].split()).astype(np.float)
+                            tmp1_2 = np.array(raw[i + iBod*(num_wave_directions*num_frequencies*2) + iDir*num_frequencies*2 + iFreq*2 + 2].split()).astype(np.float)
+                            tmp2 = tmp1_1[0:3].astype(np.float).astype(np.int)
+                            if (iDir==0) and (iFreq==0):
+                                excitation_magnitude[tmp2[0]] = np.zeros([6,num_wave_directions,num_frequencies])
+                                excitation_phase[tmp2[0]] = np.zeros([6,num_wave_directions,num_frequencies])
+                            excitation_magnitude[tmp2[0]][:,tmp2[1]-1,tmp2[2]-1] = tmp1_1[3:]
+                            excitation_phase[tmp2[0]][:,tmp2[1]-1,tmp2[2]-1] = tmp1_2
+
+                            
+
+        for i in xrange(num_bodies):
+            print 'body' + str(i+1) + ':' 
+            self.data[i] = bem.HydrodynamicData() 
+
+            self.data[i].rho = density
+            self.data[i].g = gravity
+            self.data[i].wave_dir = wave_directions
+            self.data[i].num_bodies = num_bodies
+            
+            self.data[i].cg = cg[i+1]
+            self.data[i].cb = 'not_defined'
+            self.data[i].k = stiffness_matrix[i+1] #check units
+            self.data[i].T = 2*np.pi/frequencies #check units
+            self.data[i].w = frequencies #check units
+            
+            self.data[i].wp_area = 'not_defined' 
+            self.data[i].buoy_force = 'not_defined'
+            self.data[i].disp_vol = 'not_defined' #need this ...
+            self.data[i].water_depth = water_depth
+            self.data[i].body_num = i
+            
+            self.data[i].name = 'body' + str(i) #need this. Should be user-defined name ...
+            self.data[i].bem_code = code
+            self.data[i].bem_raw_data = raw
+
+            self.data[i].am.all = added_mass[i+1]
+            print '   * Setting added mass at infinite frequency to added mass at omega = ' + str(frequencies[-1])
+            self.data[i].am.inf = added_mass[i+1][:,:,-1]
+            print '   * Setting added mass at zero frequency to added mass at omega = ' + str(frequencies[0])
+            self.data[i].am.zero = added_mass[i+1][:,:,0]
+            self.data[i].rd.all = radiation_damping[i+1]
+            self.data[i].ex.mag = excitation_magnitude[i+1]
+            self.data[i].ex.phase = excitation_phase[i+1]
+            print '   * Calculating real and imaginary excitation  components.'
+            self.data[i].ex.re = self.data[i].ex.mag * np.cos(self.data[i].ex.phase)
+            self.data[i].ex.im = self.data[i].ex.mag * np.sin(self.data[i].ex.phase)
+
--- tutorials/aqwa/run.py
+++ tutorials/aqwa/run.py
@@ -1,16 +1,13 @@
-"""
-Created on Thu Jan 15 10:42:54 2015
 
-@author: mlawson
-"""
+
 from bemio.io import aqwa as aio
-from bemio.data_structures import bem as hd
+from bemio.data_structures import bem
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.interactive(True)
 
 # Load AQWA output data file
-aq = aio.AqwaOutput(out_file='./data/aqwa_example_data.lis')
+aq = aio.AqwaOutput(out_file='./data/aqwa_example_data.AH1')
 
 # Plot diag components of added mass and damping
 comps_to_plot = [[0,0],[1,1],[2,2]]
@@ -18,6 +15,15 @@ aq.data[0].plot_am_rd(comps_to_plot)
 aq.data[1].plot_am_rd(comps_to_plot)
 aq.data[2].plot_am_rd(comps_to_plot)
 
+# Calculate IRF and plot
+for i in xrange(aq.data[0].num_bodies):
+	aq.data[i].calc_irf()
+	#aq.data[i].calc_ss()
+
 # Write hydrodynamic data to HDF5 file format
-hd.write_hdf5(aq.data,aq.files['hdf5'])
-hd.write_pickle(aq.data,aq.files['pickle'])
+bem.write_hdf5(aq.data,aq.files['hdf5'])
+bem.write_pickle(aq.data,aq.files['pickle'])
+
+# Keep Python running so the user can view the plots #
+raw_input('\nPress enter to exit and close plots')
+plt.close('all')
