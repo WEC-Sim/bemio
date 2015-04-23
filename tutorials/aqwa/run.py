090688e1a518ae3bc17b2b8617cd25021312da37
Carlos A Michelen Strofer <c.michelen.strofer@gmail.com>
2015-04-23 15:40:14 -0600

    AQWA parser now reads displaced volume from list file

--- tutorials/aqwa/run.py
+++ tutorials/aqwa/run.py
@@ -4,7 +4,7 @@ from bemio.data_structures import bem
 import matplotlib.pyplot as plt
 
 # Load AQWA output data file
-aq = aio.AqwaOutput(out_file='./data/aqwa_example_data.AH1')
+aq = aio.AqwaOutput(hydro_file='./data/aqwa_example_data.AH1',list_file='./data/aqwa_example_data.LIS')
 
 # Plot diag components of added mass and damping
 comps_to_plot = [[0,0],[1,1],[2,2]]
@@ -13,8 +13,8 @@ aq.data[1].plot_am_rd(comps_to_plot)
 aq.data[2].plot_am_rd(comps_to_plot)
 
 # Calculate IRF and plot using the aqwa module
-#for i in xrange(aq.data[0].num_bodies):
-	#aq.data[i].calc_irf(t_end=50, n_t = 101, n_w=201)
+for i in xrange(aq.data[0].num_bodies):
+	aq.data[i].calc_irf()
 	#aq.data[i].calc_ss()
 
 # Write hydrodynamic data to HDF5 file format
