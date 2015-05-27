64342016bfe6dc93fe0801e0d4e521112f153518
Michael Lawson <lawsonro3@gmail.com>
2015-05-27 14:31:55 -0600

    updating aqwa stuff

--- tutorials/aqwa/run.py
+++ tutorials/aqwa/run.py
@@ -1,16 +1,10 @@
-"""
-Created on Thu Jan 15 10:42:54 2015
 
-@author: mlawson
-"""
 from bemio.io import aqwa as aio
-from bemio.data_structures import bem as hd
+from bemio.data_structures import bem
 import matplotlib.pyplot as plt
-plt.close('all')
-plt.interactive(True)
 
 # Load AQWA output data file
-aq = aio.AqwaOutput(out_file='./data/aqwa_example_data.lis')
+aq = aio.AqwaOutput(hydro_file='./data/aqwa_example_data.AH1',list_file='./data/aqwa_example_data.LIS')
 
 # Plot diag components of added mass and damping
 comps_to_plot = [[0,0],[1,1],[2,2]]
@@ -18,6 +12,17 @@ aq.data[0].plot_am_rd(comps_to_plot)
 aq.data[1].plot_am_rd(comps_to_plot)
 aq.data[2].plot_am_rd(comps_to_plot)
 
+# Calculate IRF and plot using the aqwa module
+for i in xrange(aq.data[0].num_bodies):
+	aq.data[i].calc_irf()
+	#aq.data[i].calc_ss()
+
 # Write hydrodynamic data to HDF5 file format
-hd.write_hdf5(aq.data,aq.files['hdf5'])
-hd.write_pickle(aq.data,aq.files['pickle'])
+bem.write_hdf5(aq)
+bem.write_pickle(aq)
+
+# Keep Python running so the user can view the plots #
+plt.show()
+raw_input('\nPress enter to exit and close plots')
+plt.close('all')
+
